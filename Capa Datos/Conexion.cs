a1afc5ea067628c1fab9585d705b82b8503252eb
daniiii1222 <danielarecalde999@gmail.com>
2026-08-24 12:06:06 -0300

    login interface

---
+++ Capa Datos/Conexion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace Capa_Datos
+{
+    internal class Conexion
+    {
+        private static string cadena = "Server=localhost;Database=petshopdellitoral;Uid=root;Pwd=;";
+
+        public static MySqlConnection ObtenerConexion()
+        {
+            return new MySqlConnection(cadena);
+        }
+
+    }
+}
